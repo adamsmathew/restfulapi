61832a94e8a7b7f457adb1c431ea24d11811fae3
adams <adamsmathew23@gmail.com>
2024-08-20 17:14:36 +0530

    20 august

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    const AVAILABLE_PRODUCT = 'available';
+    const UNAVAILABLE_PRODUCT = 'unavailable'; // Changed to lowercase for consistency
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id',
+    ];
+
+    public function isAvailable()
+    {
+        return $this->status == self::AVAILABLE_PRODUCT; // Changed Product::AVAILABLE_PRODUCT to self::AVAILABLE_PRODUCT
+    }
 }
